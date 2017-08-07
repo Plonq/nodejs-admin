b12322f7c782d2dc7286c77da36fb829dff41a34
Huon Imberger <s3527782@student.rmit.edu.au>
2017-08-07 16:49:26 +1000

    Bookings list page

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// For external API calls
+var request = require('request');
+
+var API_URL = 'http://localhost/api';
+
+/* Bookings index. */
 router.get('/', function(req, res, next) {
-    res.render('bookings', { nav: 'bookings', title: 'MI. Movies Admin' });
+    var bookings = [];
+    var movies = [];
+    var movie_sessions = [];
+    request.get(API_URL+'/bookings', function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            bookings = JSON.parse(body);
+
+            request.get(API_URL+'/movies', function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    movies = JSON.parse(body);
+
+                    res.render('bookings/index', {
+                        nav: 'bookings',
+                        title: 'MI. Movies Admin',
+                        bookings: bookings,
+                        movies: movies,
+                        movie_sessions: movie_sessions
+                    });
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
